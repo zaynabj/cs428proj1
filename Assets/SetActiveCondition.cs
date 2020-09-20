1f5c5c000efd6a388ab2f9a5ad9b7006621c65a4
zaynabj <43301520+zaynabj@users.noreply.github.com>
2020-09-19 20:29:31 -0500

    project submission

---
+++ Assets/SetActiveCondition.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveCondition : MonoBehaviour
+{
+    public List <GameObject> conditions = new List<GameObject>();
+    public GameObject sunny;
+    public GameObject fewClouds;
+    public GameObject scatteredClouds;
+    public GameObject brokenClouds;
+    public GameObject showers;
+    public GameObject rain;
+    public GameObject thunderstorm;
+    public GameObject snow;
+    public GameObject mist;
+    private int currIndex;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        conditions.Add(sunny);
+        conditions.Add(fewClouds);
+        conditions.Add(scatteredClouds);
+        conditions.Add(brokenClouds);
+        conditions.Add(showers);
+        conditions.Add(rain);
+        conditions.Add(thunderstorm);
+        conditions.Add(snow);
+        conditions.Add(mist);
+
+        fewClouds.SetActive(false);
+        scatteredClouds.SetActive(false);
+        brokenClouds.SetActive(false);
+        showers.SetActive(false);
+        rain.SetActive(false);
+        thunderstorm.SetActive(false);
+        snow.SetActive(false);
+        mist.SetActive(false);
+        currIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow)){
+            conditions[currIndex].SetActive(false);
+            if (currIndex > 0){
+                currIndex--;
+            }
+            else{
+                currIndex = 8;
+            }
+            Debug.Log("\ncurrIndex: " + currIndex);
+            Debug.Log("\nActive object: " + conditions[currIndex]);
+      
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            Debug.Log("Right arrow pressed");
+            conditions[currIndex].SetActive(false);
+            if (currIndex < 8)
+                currIndex++;
+            else 
+                currIndex = 0;
+            //Debug.Log("Curr Index is: " + currIndex);
+            Debug.Log("\ncurrIndex: " + currIndex);
+            Debug.Log("\nActive object: " + conditions[currIndex]);
+        }
+        conditions[currIndex].SetActive(true);
+        //Debug.Log("\ncurrIndex: " + currIndex);
+        //Debug.Log("\nActive object: " + conditions[currIndex]);
+
+    }
+}
---
+++ cs428proj1-master/Assets/WeatherAPIScript.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using TMPro;
+
+public class WeatherAPIScript : MonoBehaviour
+{
+    public GameObject weatherTextObject;
+    public GameObject TemperatureTextObject;
+    public GameObject HumidityTextObject;
+    public GameObject WindSpeedTextObject;
+    string url = "http://api.openweathermap.org/data/2.5/weather?q=Chicago&APPID=556b8026b9546d89209a4933242e909b&units=imperial";
+
+   
+    void Start()
+    {
+
+    // wait a couple seconds to start and then refresh every 900 seconds
+
+       Debug.Log("I am alive!");
+       InvokeRepeating("GetDataFromWeb", 2f, 900f);
+   }
+
+   void GetDataFromWeb()
+   {
+
+       StartCoroutine(GetRequest(url));
+   }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(": Error: " + webRequest.error);
+            }
+            else
+            {
+                // print out the weather data to make sure it makes sense
+                Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
+                string weatherInfo = webRequest.downloadHandler.text;
+
+                //Set Temperature Game Object
+                int temp = weatherInfo.IndexOf("temp") + "temp::".Length;
+                Debug.Log(":\n index of temp: " + temp);
+                string temperature = weatherInfo.Substring(temp, 2);
+                Debug.Log(temperature);
+                TemperatureTextObject.GetComponent<TextMeshPro>().text = temperature;
+
+                //Set Humidity Game Object
+                int humid = weatherInfo.IndexOf("humidity") + "humidity::".Length;
+                Debug.Log(":\n index of humidity: " + humid);
+                string humidity = weatherInfo.Substring(humid, 2);
+                Debug.Log(humidity);
+                HumidityTextObject.GetComponent<TextMeshPro>().text = "Humidity: " + humidity + "%";
+
+                //Set Wind Speed Game Object
+                int speed = weatherInfo.IndexOf("speed") + "speed::".Length;
+                Debug.Log(":\n index of speed: " + speed);
+                string windSpeed = weatherInfo.Substring(speed, 4);
+                Debug.Log(windSpeed);
+                WindSpeedTextObject.GetComponent<TextMeshPro>().text = "Wind Speed: " + windSpeed + "mph";
+
+
+                //Set Wind Direction Game Object
+
+            }
+        }
+    }
+}
